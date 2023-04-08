45268f2052ab00db37b27f38f606dfbec5fc351f
Rencas <renzo.c@ohecommerce.agency>
2023-04-08 11:54:36 -0500

    cotizador use hooks with useCallback + useRef for freeze states

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,14 +1,17 @@
-import { MARCAS } from "../constants";
+import { useCallback, useRef } from "react";
+import { MARCAS, PLANS } from "../constants";
 import useCotizador from "../hooks/useCotizador"
 
 export default function Resultado() {
    const { result, datos } = useCotizador();
    const { marca, plan, year } = datos;
+   const yearRef = useRef(year);
 
    if (result === 0) return;
 
-   const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca));
-   console.log(nombreMarca)
+   const [nombreMarca] = useCallback(MARCAS.filter(m => m.id === Number(marca)), [result]);
+   const [nombrePlan] = useCallback(PLANS.filter(p => p.id === Number(plan)), [result]);
+
 
    return (
       <div className="bg-gray-100 text-center mt-5 p-5 shadow">
@@ -17,6 +20,18 @@ export default function Resultado() {
          </h2>
          <p className="my-2">
             <span className="font-bold">Marca:</span> {nombreMarca.nombre}
+         </p>
+
+         <p className="my-2">
+            <span className="font-bold">Plan:</span> {nombrePlan.nombre}
+         </p>
+
+         <p className="my-2">
+            <span className="font-bold">Año del auto:</span> {yearRef.current}
+         </p>
+
+         <p className="my-2 text-2xl">
+            <span className="font-bold">Total cotización:</span> {result}
          </p>
       </div>
    )
