aebc77ab1afa9b7611cedc524b83e321e5510fe8
Rencas <renzo.c@ohecommerce.agency>
2023-04-08 11:36:53 -0500

    cotizador calculate total to pay + add spinner + show result

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,32 @@
 export const getDifferenceYear = (year) => {
   return new Date().getFullYear() - year;
 };
+
+export const calculateMark = (marca) => {
+  let incremento;
+
+  if (marca === '1') {
+    incremento = 1.3;
+  }
+
+  if (marca === '2') {
+    incremento = 1.15;
+  }
+
+  if (marca === '3') {
+    incremento = 1.05;
+  }
+
+  return incremento;
+};
+
+export const calculatePlan = (plan) => {
+  return plan === '1' ? 1.2 : 1.5;
+};
+
+export const formatMoney = (cantidad) => {
+  return cantidad.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+};
